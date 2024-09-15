382605874591354bd2c9a45f121ac2286f5a82ab
Siddami <dharmmy621@gmail.com>
2024-09-15 11:56:39 +0100

    feat-set up page structure for chat

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,10 +1,16 @@
-//import React from 'react'
 
 export default function Chat() {
   return (
     <div>
-      <h1 className="text-[#555657] font-semibold text-2xl leading-[48px] mb-5">CHAT</h1>
-      <div></div>
+        <h1>Chat</h1>
+        <div>
+            <section>
+
+            </section>
+            <section>
+              
+            </section>
+        </div>
     </div>
   )
 }
