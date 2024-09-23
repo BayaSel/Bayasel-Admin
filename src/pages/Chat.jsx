e4b1eec3863d8c634c64cbca0109a34872bcc1a4
Siddami <dharmmy621@gmail.com>
2024-09-23 14:08:56 +0100

    dev chat structure

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -4,7 +4,7 @@ export default function Chat() {
   return (
     <div>
       <h1 className="text-[#555657] font-semibold text-2xl leading-[48px] mb-5">CHAT</h1>
-      <div className="flex justify-between">
+      <div className="flex justify-around">
           <section>Chats Section</section>
           <section>Chat Box</section>
       </div>
