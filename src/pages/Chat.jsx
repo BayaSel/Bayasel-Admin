4487060fb58c6e8bab34a4bf4a1c1d848137c8e6
Siddami <dharmmy621@gmail.com>
2024-10-02 15:49:35 +0100

    feat- worked on header section of the chat box

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -33,19 +33,19 @@ export default function Chat() {
   };
 
   return (
-    <div className="bg-customGray p-6">
+    <div>
       <h1 className="text-[#555657] font-semibold text-2xl leading-[48px] mb-5">CHAT</h1>
-      <div className="flex justify-between gap-6">
+      <div className="flex justify-between gap-6 p-6 bg-[#E7EDE8]">
         {/* Left side chat list */}
-        <section className="w-1/3 bg-white p-4 border rounded">
+        <section className="w-1/3 p-4 rounded">
           <h2 className="text-xl font-bold text-neutral mb-4">BayaSel</h2>
           <ul>
             {chats.map((chat) => (
               <li
                 key={chat.id}
                 className={`p-2 cursor-pointer mb-3 flex items-center gap-4 ${
-                  activeChat?.id === chat.id ? "bg-green-500 text-white" : "bg-gray-100"
-                } rounded`}
+                  activeChat?.id === chat.id ? "bg-green-500 text-white" : "bg-[none]"
+                }`}
                 onClick={() => setActiveChat(chat)}
               >
                 <img src={chat.image} alt={`${chat.name} image`} className="w-10 h-10 rounded-full" />
@@ -56,13 +56,20 @@ export default function Chat() {
         </section>
 
         {/* Right side chat box */}
-        <section className="w-2/3 bg-white p-4 border rounded">
+        <section className="w-2/3 bg-white border rounded">
           {activeChat ? (
             <div className="chat-box">
               {/* Chat header */}
-              <div className="flex items-center gap-4 mb-4">
+              <div className="flex justify-around items-center gap-4 mb-4 bg-Green p-2">
                 <img src={activeChat.image} alt={`${activeChat.name} image`} className="w-10 h-10 rounded-full" />
-                <h3 className="text-lg font-semibold">{activeChat.name}</h3>
+                <h3 className="text-lg font-semibold text-white">{activeChat.name}</h3>
+                <i class='bx bx-phone text-white text-3xl'></i>
+              </div>
+
+              <div className="flex items-center justify-evenly text-gray-500 gap-2">
+                  <span className="border-t border-t-5 flex-grow"></span>
+                  <p>Thu, 21</p>
+                  <span className="border-t border-t-5 flex-grow"></span>
               </div>
 
               {/* Chat messages */}
@@ -71,7 +78,7 @@ export default function Chat() {
                   <p
                     key={index}
                     className={`mb-2 p-2 rounded max-w-[70%] ${
-                      message.isUser ? "ml-auto bg-blue-500 text-white" : "mr-auto bg-gray-200 text-black"
+                      message.isUser ? "ml-auto bg-Green text-white" : "mr-auto bg-gray-200 text-black"
                     }`}
                   >
                     {message.text}
